990da32dc617db58545fe53ec3f48e82a0742ec2
Ray Villalobos <ray@planetoftheweb.com>
2015-05-17 12:07:15 -0400

    changes

--- albumtracker/app/models/album.rb
+++ albumtracker/app/models/album.rb
@@ -1,4 +1,8 @@
 class Album < ActiveRecord::Base
-    belongs_to :label
-    has_and_belongs_to_many :artist
+  belongs_to :label
+  has_and_belongs_to_many :artist
+  validates :album_title, presence: true
+  validates :genre, presence: true
+  validates :year_released, presence: true
+  validates :times_played, presence: true
 end
